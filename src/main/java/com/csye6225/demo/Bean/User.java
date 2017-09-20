59770db1de26e99c52d0b69d725ca4886c4c825d
Yuting Jing <jing.yu@husky.neu.edu>
2017-09-20 11:47:06 -0400

    controller test

---
+++ src/main/java/com/csye6225/demo/Bean/User.java
@@ -0,0 +1,34 @@
+package com.csye6225.demo.Bean;
+
+public class User {
+    String name;
+    String phone;
+    String email;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/csye6225/demo/controllers/HomeController.java
+++ src/main/java/com/csye6225/demo/controllers/HomeController.java
@@ -6,14 +6,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.csye6225.demo.Bean.User;
+import com.google.gson.Gson;
 
 import com.google.gson.JsonObject;
 
 /**
  * SHIRUI_WANG,001226459, wang.shirui@husky.neu.edu
  * WENHE_MA, 001238705, ma.wenhe@husky.neu.edu
- * YUTING_JING, 0012 , jing.yu@husky.neu.edu
+ * YUTING_JING, 001221590 , jing.yu@husky.neu.edu
+ * HAOAN_YAN,0012
  */
 
 @Controller
@@ -31,4 +35,20 @@ public class HomeController {
     return jsonObject.toString();
   }
 
+  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "text/plain")
+  @ResponseBody
+  public String login() {
+
+
+    return "this is login!";
+  }
+
+  @RequestMapping(value = "/logout", method = RequestMethod.GET, produces = {"text/plain", "application/*"})
+  @ResponseBody
+  public String logout(@RequestParam String userStr) {
+    Gson gson = new Gson();
+    User user = gson.fromJson(userStr,User.class);
+    return "Hi, "+user.getName()+", this is logout";
+//    return "Hi, "+userStr+", this is logout";
+  }
 }
