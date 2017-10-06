51de32b0cff6fb041aac5f5e6867f34cbf124810
hailie <hailie@hailiedeMacBook-Pro.local>
2017-10-06 15:06:05 -0400

    add scripts

---
+++ infrastructure/scripts/delete-csye6225-cloudformation-stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#delete a stack instance
+
+instanceId=$(aws cloudformation describe-stack-resources --stack-name "mystack" | jq -r '.StackResources[0].PhysicalResourceId')
+
+#enable-api-termination
+aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $instanceId
+
+#delete the stack named mystack
+aws cloudformation delete-stack --stack-name mystack
