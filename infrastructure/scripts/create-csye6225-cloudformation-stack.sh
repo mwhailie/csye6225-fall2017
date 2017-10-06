b89080528c96b74ae5349231cf8d71f753510d66
hailie <hailie@hailiedeMacBook-Pro.local>
2017-10-06 15:49:27 -0400

    create stack pass parameter

--- infrastructure/scripts/create-csye6225-cloudformation-stack.sh
+++ infrastructure/scripts/create-csye6225-cloudformation-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #create a stack instance
 
-aws cloudformation create-stack --stack-name mystack --template-body file:///home/hailie/Documents/csye6225/Assignment4/csye6225-spring-boot-starter-webapp/infrastructure/cloudformation/simple-ec2-instance-securitygroup-cloudformation-stack.json
+aws cloudformation create-stack --stack-name $1 --template-body file:///home/hailie/Documents/csye6225/Assignment4/csye6225-spring-boot-starter-webapp/infrastructure/cloudformation/simple-ec2-instance-securitygroup-cloudformation-stack.json
 
