a2f4edafd3839d8f59bca34002c30fcfc5d1eb3b
hailie <hailie@hailiedeMacBook-Pro.local>
2017-10-07 17:44:16 -0400

    change hardcode

--- infrastructure/scripts/delete-csye6225-cloudformation-stack.sh
+++ infrastructure/scripts/delete-csye6225-cloudformation-stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #delete a stack instance
 
-instanceId=$(aws cloudformation describe-stack-resources --stack-name "mystack" | jq -r '.StackResources[0].PhysicalResourceId')
+instanceId=$(aws cloudformation describe-stack-resources --stack-name $1 | jq -r '.StackResources[0].PhysicalResourceId')
 
 #enable-api-termination
 aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $instanceId
