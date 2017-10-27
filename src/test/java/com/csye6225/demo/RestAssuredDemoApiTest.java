bfad1e11142dce7dffdaedf57962ba19c8d2d23b
Shirui Wang <wangshirui19940202@hotmail.com>
2017-10-27 15:36:26 -0400

    hhhh

--- src/test/java/com/csye6225/demo/RestAssuredDemoApiTest.java
+++ src/test/java/com/csye6225/demo/RestAssuredDemoApiTest.java
@@ -18,6 +18,7 @@ public class RestAssuredDemoApiTest {
 
   @Test
   public void testGetHomePage() throws URISyntaxException {
+    System.out.println("test home page ");
     // RestAssured.when().get(new URI("http://localhost:8080/")).then().statusCode(200);
   }
 
