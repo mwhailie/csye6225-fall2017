63b904217a03947d2e9e9d6a3caa50ae53e79f0e
yuting <jing.yu@husky.neu.edu>
2017-12-15 15:49:21 -0500

    final_instance_database_cloudwatch

--- infrastructure/gcp/scripts/create-deployment-manager.sh
+++ infrastructure/gcp/scripts/create-deployment-manager.sh
@@ -5,4 +5,6 @@ projectID=$(gcloud config list --format 'value(core.project)')
 
 gcloud deployment-manager deployments update my-first-deployment --config ../configuration/vm.yaml 
 
-gcloud deployment-manager deployments list 
+
+
+# gcloud deployment-manager deployments list 
