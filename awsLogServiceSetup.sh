e13e5aa8f2d2e925f37b3b664b3285a1c42040a8
hailie <ma.wenhe@husky.neu.edu>
2017-12-15 23:57:37 -0500

    cw

--- awsLogServiceSetup.sh
+++ awsLogServiceSetup.sh
@@ -3,3 +3,4 @@ cd /home/ubuntu
 wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
 sudo python ./awslogs-agent-setup.py -n -r us-east-1 --region us-east-1 -c ./awslogs.conf
 sudo service awslogs start
+sudo systemctl enable awslogs.service
